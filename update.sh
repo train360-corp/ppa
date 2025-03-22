b6316011916ca2610072567a99a7c8155e3a4b57
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-03-22 15:47:21 -0400

    feat: add update.sh

--- update.sh
+++ update.sh
@@ -3,24 +3,24 @@
 set -e
 set -v
 
+cd "$(dirname "$0")"
+HERE="$(pwd)"
+
 export KEYNAME=2708FE95C2F86BA66026C853E47562C3606A0EF4
 
 (
     set -e
     set -v
 
-    cd ./packages/
+    cd "$HERE/packages"
 
-    # Packages & Packages.gz
     dpkg-scanpackages --multiversion . > Packages
     gzip -k -f Packages
 
-    # Release, Release.gpg & InRelease
     apt-ftparchive release . > Release
     gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
     gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
 
-    # Sign
     gpg --yes --clearsign -o InRelease Release
     gpg --yes -abs -o Release.gpg Release 
 )
